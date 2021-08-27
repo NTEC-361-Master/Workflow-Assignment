2d21821196ca2a94cf01498bf1ed507d9a2cc444
Bill Baker <bill@baker88.com>
2021-08-27 10:52:18 -0700

    Initial version

---
+++ hello.py
@@ -0,0 +1,15 @@
+# hello.py
+"""
+NTEC 361
+date: <ex: mm/dd/yyyy>
+<your name>
+Name: <assignment/lab name>
+Description: <assignment/lab description>
+"""
+
+def main():
+    print()
+    print("Hello")
+    print()
+    
+main()    
